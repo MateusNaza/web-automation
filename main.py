0540c90242212f2b534fff9f7d70a3d13fe4e4ec
Mateus Nazário <127886025+MateusNaza@users.noreply.github.com>
2025-06-17 13:30:28 +0000

    feat: integra o script transform com o main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from extract import extract_html
 from parser import parser
+from transform import load_to_csv
 
 url = 'https://ge.globo.com/futebol/brasileirao-serie-b/'
 
@@ -11,7 +12,11 @@ html_dict = extract_html(url)
 json_data = {rodada: parser(html) for rodada, html in html_dict.items()}
 
 # Salva o json
-with open("resultados.json", "w", encoding="utf-8") as f:
+json_file_name = 'resultados.json'
+with open(json_file_name, 'w', encoding='utf-8') as f:
     json.dump(json_data, f, ensure_ascii=False, indent=4)
 
-print("Arquivo JSON criado com sucesso!")
+print('Arquivo JSON criado com sucesso!')
+
+# Transforma e carrega o arquivo em CSV
+load_to_csv(json_file_name)
--- transform.py
+++ transform.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
-# Carrega o JSON em um dataframe
-df = pd.read_json('resultados.json')
+def load_to_csv(json):
+    # Carrega o JSON em um dataframe
+    df = pd.read_json(json)
 
-# Transforma colunas do Dataframe em Linhas
-df_melted = df.melt(var_name="Rodada", value_name="Dados")
+    # Transforma colunas do Dataframe em Linhas
+    df_melted = df.melt(var_name="Rodada", value_name="Dados")
 
-# Expande o dicionário da coluna dados, transformando as chaves em colunas
-df_expandido = df_melted["Dados"].apply(pd.Series)
+    # Expande o dicionário da coluna dados, transformando as chaves em colunas
+    df_expandido = df_melted["Dados"].apply(pd.Series)
 
-# Adicionar a coluna de Rodada de volta
-df_expandido["Rodada"] = df_melted["Rodada"]
+    # Adicionar a coluna de Rodada de volta
+    df_expandido["Rodada"] = df_melted["Rodada"]
 
-df_expandido.to_csv('tabela.csv')
+    df_expandido.to_csv('tabela.csv')
